be39f49128cc29ebd7f499df6ec3c41fec601901
Aya E Mahmoud <ayae.mahmoud@gmail.com>
2024-03-23 01:11:22 +0200

    Add some of the operation functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,14 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var displayLabel: UILabel!
-    
-    
+    @IBOutlet weak private var displayLabel: UILabel!
+    private var didFinishTyping: Bool = true
+    private var isDecimalPointAdded: Bool = false
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
     
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Could not convert \(String(describing: displayLabel.text)) to double!")
+        }
+        if let operation = sender.currentTitle {
+            if displayLabel.text == "0" {
+                return
+            } else {
+                if operation == "+/-" {
+                    displayLabel.text = String(number * -1)
+                } else if operation == "%" {
+                    displayLabel.text = String(number * 0.01)
+                } else if operation == "AC" {
+                    displayLabel.text = "0"
+                    didFinishTyping = true
+                    isDecimalPointAdded = false
+                }
+            }
+        }
     }
 
     
@@ -25,6 +43,22 @@ class ViewController: UIViewController {
         
         //What should happen when a number is entered into the keypad
     
+        if let numVal = sender.currentTitle {
+            if didFinishTyping {
+                displayLabel.text = sender.currentTitle
+                didFinishTyping = false
+            } else {
+                if numVal == "." {
+                    if !isDecimalPointAdded {
+                        //Add decimal point for the first time
+                        displayLabel.text! += numVal
+                        isDecimalPointAdded = true
+                    }
+                } else {
+                    displayLabel.text! += numVal
+                }
+            }
+        }
     }
 
 }
